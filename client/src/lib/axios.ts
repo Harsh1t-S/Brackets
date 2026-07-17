b565ce6b281ce25bba7c4b8f2684eba0da2aca1e
JPS133 <jaiprataps830@gmail.com>
2026-07-17 19:33:42 +0530

    Change API base URL for axios instance

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://codeforge-production-c28d.up.railway.app/api",
+  baseURL: "https://brackets-production-26e2.up.railway.app/",
   headers: {
     "Content-Type": "application/json",
   },
